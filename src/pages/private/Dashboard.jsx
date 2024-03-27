e61df3213ad3b1395587ccc3de5ae43acd2fb13b
Henry Gustavo X Az <azhenrygus@outlook.es>
2024-03-27 14:10:32 -0600

    feature add dashboard info

--- src/pages/private/Dashboard.jsx
+++ src/pages/private/Dashboard.jsx
@@ -1,9 +1,23 @@
 import LoggedIn from '@/layouts/LoggedIn';
+import { useEffect, useState } from 'react';
 
 export const Dashboard = () => {
+  const [user, setUser] = useState({})
+
+  const ROLES = {
+    'user': 'empleado',
+    'admin': 'Administrador de Empresa',
+  }
+
+  useEffect(() => {
+    setUser(JSON.parse(sessionStorage.getItem('data-user')))
+
+  }, [])
+
   return (
     <LoggedIn>
-      <h1>Bienvenido al Dashboard (private)</h1>
+      <h1 className='text-center font-bold text-xl'>{user.username}</h1>
+      <p className='text-center'>Bienvenido eres un {ROLES[user.role]}</p>
     </LoggedIn>
   )
 }
